00dc3fd15458d5523ad292b7d2ce19c71397a0e2
Agmatt <agumbamats@gmail.com>
2026-03-06 09:35:19 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -51,7 +51,7 @@ const App = () => {
               </li>
               <li>
                 <a
-                  href='/careers'
+                  href='/get-involved/careers' target='_blank'
                   className='hover:text-white transition-colors duration-300'>
                   Careers
                 </a>
