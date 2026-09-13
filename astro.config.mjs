45045279695639e21e510cf18ecd12aa10636a15
Agmatt <agumbamats@gmail.com>
2026-09-13 08:06:09 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import alpinejs from '@astrojs/alpinejs';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import netlify from '@astrojs/netlify';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://spmh.co.ke', // <--- Add this line right here
   output: 'server',
   adapter: netlify(),
   vite: {
